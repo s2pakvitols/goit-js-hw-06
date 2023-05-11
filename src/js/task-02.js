a5ea9cf6bc896e3c92a9cfac172cbcb2ee220e57
s2pakvitols <s2pakvitols@gmail.com>
2023-05-11 14:27:49 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.forEach((element) => {
+  const newElementEl = document.createElement("li");
+  newElementEl.textContent = element;
+  newElementEl.className = "item";
+  const refIngredientsEl = document.querySelector("#ingredients");
+
+  refIngredientsEl.append(newElementEl);
+});
+
+/*не можу зрозуміти, як додати елементи за один раз */
