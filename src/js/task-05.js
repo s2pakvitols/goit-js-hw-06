cfdb7cc0c8e7187b9a79d062f18153edad6599f8
s2pakvitols <s2pakvitols@gmail.com>
2023-05-12 21:10:22 +0200

    task05

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,7 +2,7 @@ const formEl = document.querySelector("#name-input");
 const spanEl = document.querySelector("#name-output");
 formEl.addEventListener("input", (event) => {
   console.log(event.currentTarget.value);
-    if (!event.currentTarget.value) {
+    if (event.currentTarget.value) {
       spanEl.textContent = event.currentTarget.value;
   } else {
     spanEl.textContent = "Anonymous";
