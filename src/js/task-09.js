47f56df334c76576622639e05102f6735f25cd1e
s2pakvitols <s2pakvitols@gmail.com>
2023-05-13 18:44:22 +0200

    task09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+const buttonChangeEl = document.querySelector(".change-color");
+const spanColorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonChangeEl.addEventListener("click", handleSubmit);
+
+function handleSubmit(event) {
+bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  spanColorEl.textContent = `${getRandomHexColor()}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
